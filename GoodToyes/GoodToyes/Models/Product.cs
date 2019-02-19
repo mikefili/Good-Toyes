eb307dddfe331ff3253cdf5fd28bc07eb69e21b1
Jason Few <omence11@gmail.com>
2019-02-19 09:22:42 -0800

    Added model for DB Schema

---
+++ GoodToyes/GoodToyes/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodToyes.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Number { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
