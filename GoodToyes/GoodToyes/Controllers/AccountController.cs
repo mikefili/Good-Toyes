53b90351d1e609f3cee12c7128129a3422f5cbc4
Jason Few <omence11@gmail.com>
2019-02-20 13:27:25 -0800

    Account Controller Done

--- GoodToyes/GoodToyes/Controllers/AccountController.cs
+++ GoodToyes/GoodToyes/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GoodToyes.Models;
+using GoodToyes.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,18 +11,49 @@ namespace GoodToyes.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        
         /// <summary>
         /// User register Page
         /// </summary>
         /// <returns>View</returns>
         [HttpGet]
         public IActionResult Register() => View();
-        
-        [HttpPost]
-        public IActionResult Register()
-        {
 
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel rvm)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = rvm.Email,
+                    Email = rvm.Email,
+                    FirstName = rvm.FirstName,
+                    LastName = rvm.LastName,
+                    Birthdate = rvm.Birthdate
+                };
+
+               var result = await _userManager.CreateAsync(user, rvm.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                
+                
+            }
+            return View(rvm);
         }
-        
+
     }
 }
--- GoodToyes/GoodToyes/Models/ApplicationUser.cs
+++ GoodToyes/GoodToyes/Models/ApplicationUser.cs
@@ -13,5 +13,7 @@ namespace GoodToyes.Models
         public string LastName { get; set; }
 
         public DateTime Birthdate { get; set; }
+
+        public string Email { get; set; }
     }
 }
