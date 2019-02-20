dc89b59571fe4a173c69d0c1a352d9c1f01afe8d
Jason Few <omence11@gmail.com>
2019-02-20 12:11:15 -0800

    Identity DB built

---
+++ GoodToyes/GoodToyes/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodToyes.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// User register Page
+        /// </summary>
+        /// <returns>View</returns>
+        [HttpGet]
+        public IActionResult Register() => View();
+        
+        [HttpPost]
+        public IActionResult Register()
+        {
+
+        }
+        
+    }
+}
---
+++ GoodToyes/GoodToyes/Migrations/20190220194415_ini.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GoodToyes.Migrations
+{
+    public partial class ini : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
