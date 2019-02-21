87c1a0f051aff8bada4dfdcf8b33243ca6fc8002
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-20 17:32:32 -0800

    interface implemented in ProductManager service

--- GoodToyes/GoodToyes/Models/Services/ProductManager.cs
+++ GoodToyes/GoodToyes/Models/Services/ProductManager.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using GoodToyes.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GoodToyes.Models.Services
 {
-    interface ProductManager
+    public class ProductManager : IProduct
     {
+        public Task CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
