473473a657623a04a47506e343b2837c9eec6159
Jason Few <omence11@gmail.com>
2019-02-26 11:59:45 -0800

    cart and cartitems models created

---
+++ GoodToyes/GoodToyes/Models/Interfaces/ICart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodToyes.Models.Interfaces
+{
+    public interface ICart
+    {
+        // Saves to Cart
+        Task SaveToCart(Product product);
+
+        // Edit Purchas Details
+        Task<Product> EditPurchas(int id);
+
+        // Get all products in cart
+        Task<IEnumerable<Product>> GetProducts();
+
+
+        // Delete from item from cart
+        Task DeleteProduct(int id);
+    }
+}
