6770c60e4432dd937bf76cb3727fc0c8a120e8aa
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-26 21:46:54 -0800

    used dependency injection to bring in user manager, sign in manager, and cart context to the shop contoller

--- GoodToyes/GoodToyes/Controllers/ShopController.cs
+++ GoodToyes/GoodToyes/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using GoodToyes.Models.Interfaces;
+﻿using GoodToyes.Models;
+using GoodToyes.Models.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,10 +12,14 @@ namespace GoodToyes.Controllers
     public class ShopController : Controller
     {
         private readonly IProduct _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public ShopController(IProduct context)
+        public ShopController(IProduct context, ICart cart, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
