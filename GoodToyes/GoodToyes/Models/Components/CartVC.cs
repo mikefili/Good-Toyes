dc03828f1c6a335fc64c07b54d9bccae5a26edcf
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-27 17:10:15 -0800

    Set up view component

---
+++ GoodToyes/GoodToyes/Models/Components/CartVC.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GoodToyes.Models.Interfaces;
+using GoodToyes.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodToyes.Models.Components
+{
+    public class Cart : ViewComponent
+    {
+        private GoodToyesDbContext _context;
+
+        public Cart(GoodToyesDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cartItems = await _context.CartItems.OrderByDescending(ci => ci.ID).ToListAsync();
+            return View(cartItems);
+        }
+    }
+}
