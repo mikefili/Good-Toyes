c106d5bd9257e5762218a2c957594180fdef6e0d
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-27 18:20:32 -0800

    nuked app user view model, replaced with cart item view model

--- GoodToyes/GoodToyes/Models/Components/CartVC.cs
+++ GoodToyes/GoodToyes/Models/Components/CartVC.cs
@@ -6,22 +6,41 @@ using System.Threading.Tasks;
 using GoodToyes.Models.Interfaces;
 using GoodToyes.Data;
 using Microsoft.EntityFrameworkCore;
+using GoodToyes.ViewModels;
 
 namespace GoodToyes.Models.Components
 {
     public class CartVC : ViewComponent
     {
         private GoodToyesDbContext _context;
+        private ICart _cart;
+        private IProduct _product;
 
-        public CartVC(GoodToyesDbContext context)
+        public CartVC(GoodToyesDbContext context, ICart cart, IProduct product)
         {
             _context = context;
+            _cart = cart;
+            _product = product;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var cartItems = await _context.CartItems.OrderBy(ci => ci.ID).ToListAsync();
-            return View(cartItems);
+            var user = User.Identity.Name;
+            var userCart = await _cart.GetCart(user);
+            var userItems = userCart.CartItems.ToList();
+            List<CartItemViewModel> cartItemVMList = new List<CartItemViewModel>();
+
+            foreach (var item in userItems)
+            {
+                Product product = await _product.GetProduct(item.ProductID);
+                CartItemViewModel cartItemVM = new CartItemViewModel();
+                cartItemVM.ProductName = product.Name;
+                cartItemVM.ProductPrice = product.Price;
+                cartItemVM.Quantity = product.Quantity;
+                cartItemVMList.Add(cartItemVM);
+            }
+
+            return View(cartItemVMList);
         }
     }
 }
