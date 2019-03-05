061604e8ca71b1f1d20b5c298a48647b41a58d31
Michael Filicetti <filicetti.michael@gmail.com>
2019-03-05 10:51:43 -0800

    added application roles to RoleInitializer class

--- GoodToyes/GoodToyes/Models/RoleInitializer.cs
+++ GoodToyes/GoodToyes/Models/RoleInitializer.cs
@@ -10,7 +10,8 @@ namespace GoodToyes.Models
     {
         private static readonly List<IdentityRole> Roles = new List<IdentityRole>()
         {
-            new IdentityRole{Name = ApplicationRoles.Member}
+            new IdentityRole{Name = ApplicationRoles.Member, NormalizedName = ApplicationRoles.Member.ToUpper(), ConcurrencyStamp = Guid.NewGuid().ToString() },
+            new IdentityRole{Name = ApplicationRoles.Admin, NormalizedName = ApplicationRoles.Admin.ToUpper(), ConcurrencyStamp = Guid.NewGuid().ToString() }
         };
     }
 }
