a77ee05905c4baa6fae3ed30d891a0adc31a7dcf
Michael Filicetti <filicetti.michael@gmail.com>
2019-03-05 10:41:38 -0800

    added user profile and admin razor pages

--- GoodToyes/GoodToyes/Migrations/20190305183855_initial.Designer.cs
+++ GoodToyes/GoodToyes/Migrations/20190305183855_initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GoodToyes.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20190304192306_initial")]
+    [Migration("20190305183855_initial")]
     partial class initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
