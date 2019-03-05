da462d233ce361955c498e29c42bfaa0edba60fe
Michael Filicetti <filicetti.michael@gmail.com>
2019-03-05 10:48:21 -0800

    added ApplicationRoles class to ApplicationUser class

--- GoodToyes/GoodToyes/Models/ApplicationUser.cs
+++ GoodToyes/GoodToyes/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,18 +9,24 @@ namespace GoodToyes.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
         public DateTime Birthdate { get; set; }
 
         public bool SpayedOrNeutered { get; set; }
-
         public string StreetAddress { get; set; }
-
         public string City { get; set; }
-
         public string Zip { get; set; }
+    }
+
+    public static class ApplicationRoles
+    {
+        public const string Member = "Member";
+        public const string Admin = "Admin";
     }
 }
---
+++ GoodToyes/GoodToyes/Models/RoleInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodToyes.Models
+{
+    public class RoleInitializer
+    {
+        private static readonly List<IdentityRole> Roles = new List<IdentityRole>()
+        {
+            new IdentityRole{Name = ApplicationRoles.Member}
+        };
+    }
+}
