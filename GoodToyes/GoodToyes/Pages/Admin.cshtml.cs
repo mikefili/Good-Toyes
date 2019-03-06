de3a216074884263161be332a5be37b73e745e23
Jason Few <omence11@gmail.com>
2019-03-06 13:49:33 -0800

    Got some admin done

--- GoodToyes/GoodToyes/Pages/Admin.cshtml.cs
+++ GoodToyes/GoodToyes/Pages/Admin.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GoodToyes.Models;
+using GoodToyes.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,48 @@ namespace GoodToyes.Pages
 {
     public class AdminModel : PageModel
     {
+        private IProduct _product;
+
+        public AdminModel(IProduct product)
+        {
+            _product = product;
+        }
+
+        /// <summary>
+        /// Deletes a product
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task OnPostDelete(int id)
+        {
+            await _product.DeleteProduct(id);
+        }
+
+        /// <summary>
+        /// creates a new product
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public async Task OnPostCreateProduct(Product product)
+        {
+            await _product.CreateProduct(product);
+        }
+
+        /// <summary>
+        /// updates product
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns>updated product</returns>
+        public async Task OnPostUpdateProduct(Product product)
+        {
+            await _product.UpdateProduct(product);
+        }
+
+
         public void OnGet()
         {
         }
+
+
     }
 }
