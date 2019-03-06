27ab998cebfe912c35b133638bc99c09a26668a3
Michael Filicetti <filicetti.michael@gmail.com>
2019-03-06 09:21:27 -0800

    added scripts to scripts dir in wwwroot

---
+++ GoodToyes/GoodToyes/wwwroot/Scripts/Scripts.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    $(".navbar-burger").click(function () {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
+});
