cc7e90473af6f59c53d27351cfc2ec6c8d238012
Michael Filicetti <filicetti.michael@gmail.com>
2019-03-08 17:05:28 -0800

    Update password works!

--- GoodToyes/GoodToyes/Pages/User/UpdatePassword.cshtml.cs
+++ GoodToyes/GoodToyes/Pages/User/UpdatePassword.cshtml.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using GoodToyes.Models;
+using GoodToyes.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -14,10 +16,36 @@ namespace GoodToyes.Pages.User
         private UserManager<ApplicationUser> _userManager;
         private SignInManager<ApplicationUser> _signInManager;
 
+        [Required]
+        [BindProperty]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [BindProperty]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Does Not Match")]
+        public string ConfirmPassword { get; set; }
+
         public UpdatePasswordModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+        }
+
+        public async Task OnPostPassword()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (NewPassword == ConfirmPassword)
+            {
+                await _userManager.ChangePasswordAsync(user, Password, NewPassword);
+            } 
         }
 
         public async Task OnGet()
